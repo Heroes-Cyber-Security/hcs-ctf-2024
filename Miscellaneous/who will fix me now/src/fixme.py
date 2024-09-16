8f6b2e1b7160575f5f57d688d3abc9f44fa0e6bd
FahrulID <fahrulofficial@gmail.com>
2024-09-16 23:57:58 +0700

    feat: new chall, who will fix me now

---
+++ Miscellaneous/who will fix me now/src/fixme.py
@@ -0,0 +1,80 @@
+import os
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+import time
+import PIL
+import random
+import base64                                                                                                                                                                                                                                                                                                         ; # Remember to always use word warp kids
+
+# ONE LINER THIS USING https://jagt.github.io/python-single-line-convert/ AND THEN HIDE IT AFTER ;
+for i in range(random.randint(50, 80)):
+    cls()
+    print("{0} {1}{2}".format(base64.b64decode('''R2VuZXJhdGluZyB5b3VyIGZsYWcuLi4=''').decode("utf-8"), i, base64.b64decode('''JQ==''').decode("utf-8")))
+    time.sleep(0.1)
+f1r5t_p4r7 = base64.b64decode('''SENTezRsdzR5NV9tNGszXzV1cjNfNG55XzVjcjFwNw==''').decode("utf-8")
+print("{0}".format(base64.b64decode('''VHJhY2ViYWNrIChtb3N0IHJlY2VudCBjYWxsIGxhc3QpOg==''').decode("utf-8")))
+print("\t{2}\"{0}\"{1}".format(__file__, base64.b64decode('''LCBsaW5lIDEsIGluIDxtb2R1bGU+''').decode("utf-8"), base64.b64decode('''RmlsZSA=''').decode("utf-8")))
+print("\t\t{0}".format(base64.b64decode('''V2hhdCBkb2Vzbid0IGtpbGwgeW91LiBNYWtlcyB5b3Ugd2lzaCB5b3Ugd2VyZSBkZWFkLiBHb3QgYSBob2xlIGluIG15IHNvdWwgZ3Jvd2luZyBkZWVwZXIgYW5kIGRlZXBlci4gQW5kIEkgY2FuJ3QgdGFrZS4gT25lIG1vcmUgbW9tZW50IG9mIHRoaXMgc2lsZW5jZS4gVGhlIGxvbmVsaW5lc3MgaXMgaGF1bnRpbmcgbWUuIEFuZCB0aGUgd2VpZ2h0IG9mIHRoZSB3b3JsZCdzIGdldHRpbmcgaGFyZGVyIHRvIGhvbGQgdXAu''').decode("utf-8")))
+raise SystemExit(1) # c0mm3n7 m3 h0h0h0h0h0
+# ONE LINER THIS USING https://jagt.github.io/python-single-line-convert/ AND THEN HIDE IT AFTER ;
+
+# OBFUSCATE ME USING https://pyob.oxyry.com/
+def meaningless_function_1():
+    a = 1
+    b = 2
+    c = a + b
+    d = c - a
+    e = d * b
+    f = e / a
+    g = f ** b
+    print("{0}".format(base64.b64decode('''SXQgY29tZXMgaW4gd2F2ZXMsIEkgY2xvc2UgbXkgZXllcy4gSG9sZCBteSBicmVhdGggYW5kIGxldCBpdCBidXJ5IG1lLiBJJ20gbm90IE9LIGFuZCBpdCdzIG5vdCBhbHJpZ2h0LiBXb24ndCB5b3UgZHJhZyB0aGUgbGFrZSBhbmQgYnJpbmcgbWUgaG9tZSBhZ2Fpbg==''').decode("utf-8")))
+    return g
+
+def last_function():
+    try:
+        val = meaningless_function_1()
+        val = meaningless_function_2()
+    except Exception as e:
+        pass
+    finally:
+        An_Error_Occured # D3l3t3 m3 h3h3h3
+        pass
+
+def meaningless_function_2():
+    x = 10
+    y = 20
+    z = x + y
+    z += x
+    z -= y
+    z *= x
+    z /= y
+    return z
+
+class EmptyClass:
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+
+    def do_nothing(self):
+        pass
+
+def another_meaningless_function():
+    for i in range(10):
+        if i % 2 == 0:
+            continue
+        elif i % 3 == 0:
+            break
+        else:
+            pass
+    print("{0}".format(base64.b64decode('''V2hvIHdpbGwgZml4IG1lIG5vdz8gRGl2ZSBpbiB3aGVuIEknbSBkb3duPyBTYXZlIG1lIGZyb20gbXlzZWxmLiBEb24ndCBsZXQgbWUgZHJvd24u''').decode("utf-8")))
+
+def main():
+    s3c0nd_p4r7 = base64.b64decode('''X3kwdV9kMHdubDA0ZF8wbmwxbjNfMTVfNTRmM30=''').decode("utf-8")
+    last_function()
+    another_meaningless_function()
+    print("Heres Your Flag: ")                                                                                                                                                                                                                                                                                                                                                                                  ;print(f1r5t_p4r7 + s3c0nd_p4r7)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+# OBFUSCATE ME USING https://pyob.oxyry.com/
